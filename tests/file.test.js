d004c0e17332f242850d346fdb5c582fc32e8a23
owlsn <helloworld.zouhao@gmail.com>
2019-11-04 10:58:20 +0800

    format code

--- tests/file.test.js
+++ tests/file.test.js
@@ -1,42 +1,47 @@
-var file = require('../lib/file');
+var file = require('../lib/file')
 
-describe('file：功能测试', function() {
+describe('file：功能测试', function () {
+  // writeFile功能测试
+  describe('file.writeFile', function () {
+    test('写文件：成功', function () {
+      var path = 'temp/write-success.txt'
+      var data = '说明文档'
+      return file.writeFile(path, data).then(function (flag) {
+        expect(flag).toEqual(true)
+      })
+    })
 
-    //writeFile功能测试
-    describe('file.writeFile', function() {
+    test('写文件：失败', function () {
+      var path = 'temp/write-fail.txt'
+      var data = '我是写入的数据'
+      return file.writeFile(path, data).then(
+        function () {},
+        function (err) {
+          console.log(err)
+          expect(true).toEqual(true)
+        }
+      )
+    })
+  })
 
-        test('写文件：成功', function() {
-            var path = 'temp/write-success.txt';
-            var data = '说明文档';
-            return file.writeFile(path, data).then(function(flag) {
-                expect(flag).toEqual(true);
-            });
-        });
+  // readFile功能测试
+  describe('file.readFile功能测试', function () {
+    test('读文件：成功', function () {
+      var path = 'temp/write-success.txt'
+      return file.readFile(path).then(function (data) {
+        expect(data).toEqual('说明文档')
+      })
+    })
 
-        test('写文件：失败', function() {
-            var path = 'temp/write-fail.txt';
-            var data = '我是写入的数据';
-            return file.writeFile(path, data).then(function(){}, function(err) {
-                expect(true).toEqual(true);
-            });
-        });
-    });
-
-    //readFile功能测试
-    describe('file.readFile功能测试', function() {
-
-        test('读文件：成功', function() {
-            var path = 'temp/write-success.txt';
-            return file.readFile(path).then(function(data) {
-                expect(data).toEqual('说明文档');
-            });
-        });
-
-        test('读文件：失败', function() {
-            var path = 'temp/write-fail.txt';
-            return file.readFile(path).then(function(){}, function(err) {
-                expect(true).toEqual(true);
-            });
-        });
-    });
-});
+    test('读文件：失败', function () {
+      var path = 'temp/write-fail.txt'
+      return file.readFile(path).then(
+        function () {},
+        function (err) {
+          console.log(err)
+          expect(true).toEqual(true)
+        }
+      )
+    })
+  })
+})
--- tests/info.test.js
+++ tests/info.test.js
@@ -1,26 +1,30 @@
-var info = require('../lib/info');
+var info = require('../lib/info')
 
-describe('info：输出测试', function() {
-    //defer对象测试
-    describe('info.log', function() {
-        test('log success', function() {
-            expect(info.log(123)).toEqual(123);
-        });
+describe('info：输出测试', function () {
+  // defer对象测试
+  describe('info.log', function () {
+    test('log success', function () {
+      expect(info.log(123)).toEqual(123)
+    })
 
-        test('info file success', function() {
-            var path = 'temp/info-success.txt';
-            var data = '测试说明';
-            return info.file(path, data).then(function(flag) {
-                expect(flag).toEqual(true);
-            });
-        });
+    test('info file success', function () {
+      var path = 'temp/info-success.txt'
+      var data = '测试说明'
+      return info.file(path, data).then(function (flag) {
+        expect(flag).toEqual(true)
+      })
+    })
 
-        test('info file fail', function() {
-            var path = 'temp/info-fail.txt';
-            var data = '我是写入的数据';
-            return info.file(path, data).then(function(){}, function(err) {
-                expect(true).toEqual(true);
-            });
-        });
-    });
-});
+    test('info file fail', function () {
+      var path = 'temp/info-fail.txt'
+      var data = '我是写入的数据'
+      return info.file(path, data).then(
+        function () {},
+        function (err) {
+          console.log(err)
+          expect(true).toEqual(true)
+        }
+      )
+    })
+  })
+})
