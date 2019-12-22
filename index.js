6e77fa90a86fb0cfac4721df5a3725bd7394e3f0
owlsn <helloworld.zouhao@gmail.com>
2019-12-22 23:46:32 +0800

    init

--- index.js
+++
@@ -1,8 +0,0 @@
-export = module.export = {}
-
-var info = require('./lib/info.js')
-var file = require('./lib/file.js')
-
-export.info = info.log;
-export.writeFile = file.writeFile;
-export.readFile = file.readFile;
--- tests/jest.test.js
+++
@@ -1,5 +0,0 @@
-var jest = require('../lib/jest')
-
-test('test jest.test_function to equal 123', () => {
-  expect(jest.test_function()).toEqual(123)
-})
