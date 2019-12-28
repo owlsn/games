39c075952c4fb791c914b535c757e6fa79f04fba
owlsn <helloworld.zouhao@gmail.com>
2019-12-28 15:48:28 +0800

    add less loader

--- src/components/header.js
+++ src/components/header.js
@@ -12,55 +12,19 @@ class TopHeader extends Component {
   render() {
     return (
       <Layout>
-        <Header
-          className="header"
-          style={{ height: '48px', padding: '0', background: '#fff' }}
-        >
-          <img
-            className="logo"
-            style={{
-              width: '32px',
-              height: '32px',
-              background: 'rgba(255, 255, 255, 0.2)',
-              margin: '8px 24px',
-              float: 'left'
-            }}
-            src={owl}
-            alt="owl"
-          />
+        <Header className="header">
+          <img className="logo" src={owl} alt="owl" />
           <Trend />
           <Search
             className="searchInput"
             size="default"
             placeholder="查询 owls"
-            style={{
-              float: 'left',
-              margin: '6px',
-              width: '25%',
-              height: '36px',
-              minWidth: '200px',
-              maxWidth: '500px'
-            }}
           />
           <a href="/">
-            <Icon
-              type="rise"
-              style={{
-                float: 'left',
-                margin: '6px',
-                fontSize: '36px'
-              }}
-            />
+            <Icon className="polular" type="rise" />
           </a>
           <a href="/">
-            <Icon
-              type="bar-chart"
-              style={{
-                float: 'left',
-                margin: '6px',
-                fontSize: '36px'
-              }}
-            />
+            <Icon type="bar-chart" className="all" />
           </a>
           <Icon
             type="caret-down"
@@ -70,32 +34,11 @@ class TopHeader extends Component {
               fontSize: '18px'
             }}
           />
-          <Avatar
-            size={36}
-            icon="user"
-            style={{
-              float: 'right',
-              margin: '6px'
-            }}
-          />
-          <Button
-            type="primary"
-            size="default"
-            style={{
-              margin: '9px',
-              float: 'right'
-            }}
-          >
+          <Avatar size={36} icon="user" className="user" />
+          <Button type="primary" size="default" className="login">
             登录
           </Button>
-          <Button
-            type="primary"
-            size="default"
-            style={{
-              margin: '9px',
-              float: 'right'
-            }}
-          >
+          <Button type="primary" size="default" className="register">
             注册
           </Button>
         </Header>
