de19dd9ebeae7ac01bff388fda49064f9816a418
owlsn <helloworld.zouhao@gmail.com>
2020-01-05 21:43:40 +0800

    rebuild

--- src/configs/router.js
+++ src/configs/router.js
@@ -1,25 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import App from '../containers/app'
-import InnerHeader from '../components/inner-header'
-import Banner from '../components/banner'
-import Index from '../containers/index'
 import { hot } from 'react-hot-loader'
 
 const HotRoute = () => (
   <Router>
     <Switch>
-      <Route path="/banner">
-        <Banner />
-      </Route>
-      <Route path="/inner_header">
-        <InnerHeader />
-      </Route>
-      <Route path="/app">
-        <App />
-      </Route>
       <Route path="/">
-        <Index />
+        <App />
       </Route>
     </Switch>
   </Router>
