886373402fd951b513397b32d4dc505b3ee53d38
harriszou <harriszou@tencent.com>
2020-01-06 18:58:25 +0800

    update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,9 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/api/**',
     proxy({
       target: 'http://localhost:8080',
-      pathRewrite: function(path, req) {
-        return path.replace('/api', '')
-      },
       changeOrigin: true,
       logLevel: 'debug'
     })
