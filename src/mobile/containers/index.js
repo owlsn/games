a56a3379b7eda71be37901417a055385def87b2a
owlsn <helloworld.zouhao@gmail.com>
2020-01-18 16:27:35 +0800

    update index

--- src/mobile/containers/index.js
+++ src/mobile/containers/index.js
@@ -4,19 +4,12 @@ import {
   Menu,
   ActivityIndicator,
   NavBar,
-  Popover,
-  Icon
+  Popover, Icon
 } from 'antd-mobile'
 
-const Item = Popover.Item
+const Item = Popover.Item;
 
-const myImg = src => (
-  <img
-    src={`https://gw.alipayobjects.com/zos/rmsportal/${src}.svg`}
-    className="am-icon am-icon-xs"
-    alt=""
-  />
-)
+const myImg = src => <img src={`https://gw.alipayobjects.com/zos/rmsportal/${src}.svg`} className="am-icon am-icon-xs" alt="" />;
 
 const data = [
   {
@@ -128,20 +121,20 @@ class Index extends Component {
   }
   state = {
     visible: true,
-    selected: ''
-  }
-  onSelect = opt => {
+    selected: '',
+  };
+  onSelect = (opt) => {
     // console.log(opt.props.value);
     this.setState({
       visible: false,
-      selected: opt.props.value
-    })
-  }
-  handleVisibleChange = visible => {
+      selected: opt.props.value,
+    });
+  };
+  handleVisibleChange = (visible) => {
     this.setState({
-      visible
-    })
-  }
+      visible,
+    });
+  };
   handleClick = e => {
     e.preventDefault() // Fix event propagation on Android
     this.setState({
@@ -202,55 +195,35 @@ class Index extends Component {
             onLeftClick={this.handleClick}
             className="top-nav-bar"
           >
-            <Popover
-              mask
-              overlayClassName="fortest"
-              overlayStyle={{ color: 'currentColor' }}
-              visible={this.state.visible}
-              overlay={[
-                <Item
-                  key="4"
-                  value="scan"
-                  icon={myImg('tOtXhkIWzwotgGSeptou')}
-                  data-seed="logId"
-                >
-                  Scan
-                </Item>,
-                <Item
-                  key="5"
-                  value="special"
-                  icon={myImg('PKAgAqZWJVNwKsAJSmXd')}
-                  style={{ whiteSpace: 'nowrap' }}
-                >
-                  My Qrcode
-                </Item>,
-                <Item
-                  key="6"
-                  value="button ct"
-                  icon={myImg('uQIYTFeRrjPELImDRrPt')}
-                >
-                  <span style={{ marginRight: 5 }}>Help</span>
-                </Item>
-              ]}
-              align={{
-                overflow: { adjustY: 0, adjustX: 0 },
-                offset: [-10, 0]
-              }}
-              onVisibleChange={this.handleVisibleChange}
-              onSelect={this.onSelect}
+            <Popover mask
+            overlayClassName="fortest"
+            overlayStyle={{ color: 'currentColor' }}
+            visible={this.state.visible}
+            overlay={[
+              (<Item key="4" value="scan" icon={myImg('tOtXhkIWzwotgGSeptou')} data-seed="logId">Scan</Item>),
+              (<Item key="5" value="special" icon={myImg('PKAgAqZWJVNwKsAJSmXd')} style={{ whiteSpace: 'nowrap' }}>My Qrcode</Item>),
+              (<Item key="6" value="button ct" icon={myImg('uQIYTFeRrjPELImDRrPt')}>
+                <span style={{ marginRight: 5 }}>Help</span>
+              </Item>),
+            ]}
+            align={{
+              overflow: { adjustY: 0, adjustX: 0 },
+              offset: [-10, 0],
+            }}
+            onVisibleChange={this.handleVisibleChange}
+            onSelect={this.onSelect}
+          >
+            <div style={{
+              height: '100%',
+              padding: '0 15px',
+              marginRight: '-15px',
+              display: 'flex',
+              alignItems: 'center',
+            }}
             >
-              <div
-                style={{
-                  height: '100%',
-                  padding: '0 15px',
-                  marginRight: '-15px',
-                  display: 'flex',
-                  alignItems: 'center'
-                }}
-              >
-                <Icon type="ellipsis" />
-              </div>
-            </Popover>
+              <Icon type="ellipsis" />
+            </div>
+          </Popover>
           </NavBar>
         </div>
         {show ? (initData ? menuEl : loadingEl) : null}
