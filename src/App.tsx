7b355413e875d833987b672a08e8f017791b8d6d
owlsn <helloworld.zouhao@gmail.com>
2022-05-20 00:00:38 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <span>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
-        </span>
+        </a>
       </header>
     </div>
   );
